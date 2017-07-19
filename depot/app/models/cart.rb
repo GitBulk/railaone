7947ec677e674e86a661d0386d5d47db09c74ab3
macbookpro8 <macbookpro8@macbookpro8s-MacBook-Pro.local>
2017-07-18 17:05:04 -0700

    staged

--- depot/app/models/cart.rb
+++ depot/app/models/cart.rb
@@ -1,2 +1,11 @@
 class Cart < ActiveRecord::Base
+    # a cart has many associated line_items.
+    # Each line item contains a reference to its cart's id.
+    # the existence of line items is dependent on the existence of the cart.
+    # if we destroy a cart, deleting it from database, we will want
+    # Rails also to destroy any line items that are associated with that cart.
+    # And now, Cart can reference line items like this
+    # cart = Cart.find( ... )
+    # puts "this is has #{cart.line_items.count} line items"
+    has_many :line_items, dependent: :destroy
 end
