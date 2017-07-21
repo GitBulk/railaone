a26560b1e698c9f0b9d5d0d76b2ccca8c281d913
gitbulk <giacatiensinh@hotmail.com>
2017-07-21 09:46:14 -0700

    validate picture's size

--- girlsapp/app/models/idea.rb
+++ girlsapp/app/models/idea.rb
@@ -1,4 +1,14 @@
 class Idea < ActiveRecord::Base
+
+    validates_processing_of :picture
+    validates :picture_size_validation
+
     # map picture field in Idea table with PictureUploader
     mount_uploader :picture, PictureUploader
+
+    private
+        def picture_size_validation
+            errors[:image] << "Should be less than 500KB" if picture.size > 0.5.megabytes
+        end
+
 end
