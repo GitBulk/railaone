210864ae1cd06730d9a07f597fcc3f81ea770958
gitbulk <giacatiensinh@hotmail.com>
2017-11-01 22:32:18 +0700

    [f] index, create, show doc

--- filecabinet/app/controllers/docs_controller.rb
+++ filecabinet/app/controllers/docs_controller.rb
@@ -1,7 +1,6 @@
 class DocsController < ApplicationController
-
   def index
-
+    @docs = Doc.all
   end
 
   def show
@@ -9,11 +8,16 @@ class DocsController < ApplicationController
   end
 
   def new
-
+    @doc = Doc.new
   end
 
   def create
-
+    @doc = Doc.new(doc_params)
+    if @doc.save
+      redirect_to @doc
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -35,6 +39,6 @@ class DocsController < ApplicationController
     end
 
     def doc_params
-
+      params.require(:doc).permit(:title, :content)
     end
 end
