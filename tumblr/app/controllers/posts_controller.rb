fbf174267eb8d3d829033e102ead30ba27c3e485
gitbulk <giacatiensinh@hotmail.com>
2017-11-13 15:56:30 +0700

    [f] add post model, create, show, index

--- tumblr/app/controllers/posts_controller.rb
+++ tumblr/app/controllers/posts_controller.rb
@@ -1,6 +1,25 @@
 class PostsController < ApplicationController
 
   def index
-
+    @posts = Post.all
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @post = Post.new(post_param)
+    @post.save
+
+    redirect_to @post
+  end
+
+  private
+    def post_param
+      params.require(:post).permit(:title, :body)
+    end
 end
